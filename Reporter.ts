feae8e12cd5cec201e3d3a933dc63fd5f02c0e44
Rajesh C <rajesh.c@reflectionsinfos.com>
2024-04-11 16:51:40 +0530

    code changes

--- Reporter.ts
+++ Reporter.ts
@@ -1,24 +1,43 @@
 import type {
     FullConfig, FullResult, Reporter, Suite, TestCase, TestResult
-  } from '@playwright/test/reporter';
-  
-  class MyReporter implements Reporter {
-    onBegin(config: FullConfig, suite: Suite) {
-      console.log(`Starting the run with ${suite.allTests().length} tests`);
-    }
-  
-    onTestBegin(test: TestCase, result: TestResult) {
-      console.log(`Starting test ${test.title}`);
-    }
-  
-    onTestEnd(test: TestCase, result: TestResult) {
-      console.log(`Finished test ${test.title}: ${result.status}`);
-    }
-  
-    onEnd(result: FullResult) {
-      console.log(`Finished the run: ${result.status}`);
-    }
-  }
+} from '@playwright/test/reporter';
 
-  export default MyReporter;
+class MyReporter implements Reporter {
+
+    passedCount: number = 0;
+    failedCount: number = 0;
+
+    // constructor(passedCount : number = 0, failedCount : number = 0){
+
+    // }
+
+    onBegin(config: FullConfig, suite: Suite) {
+        console.log(suite.titlePath);
+        console.log(`Starting the run with ${suite.allTests().length} tests`);
+    }
+
+    // onTestBegin(test: TestCase, result: TestResult) {
+    //   console.log(`Starting test ${test.title}`);
+    // }
+
+    onTestEnd(test: TestCase, result: TestResult) {
+
+        console.log(`Finished test ${test.title}: ${result.status}  `);
+        if (result.status === 'passed') {
+            this.passedCount++;
+        }
+        else if (result.status === 'failed') {
+            this.failedCount++;
+        }
+    }
+
+    onEnd(result: FullResult) {
+        // console.log(`Finished the run: ${result.status} `);
+        console.log(`Passed Count : ${this.passedCount} `);
+        console.log(`Failed Count :  ${this.failedCount}`);
+        console.log(`Total Count  :  ${this.failedCount + this.failedCount}`);
+    }
+}
+
+export default MyReporter;
 
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,23 @@
 import { test, expect } from '@playwright/test';
+const { describe } = require("node:test");
 
-test('has title', async ({ page }) => {
+describe("Add prouducts to cart and Place order", () => {
+
+test('Verify log into app', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('Verify place order', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Installations' })).toBeVisible();
+});
+
 });
